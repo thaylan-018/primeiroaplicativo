6f86acba27c731282e7cf4ab0980e8e314253c78
thaylan-018 <thaylan.santos01@etec.sp.gov.br>
2021-11-18 17:57:18 +0000

    aulaDDW-1711

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native';
 
 export default function App() {
+  
+  const [nome, setNome] = useState('');
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titulo}>Primeiro Aplicativo</Text>
+     <TextInput style={styles.campo} value={nome} placeholder="Digite seu nome"
+     onChangeText={(nome)=> SetNome(nome)}/>
       <StatusBar style="auto" />
     </View>
   );
+
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +24,31 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  titulo:{
+    fontFamily:'Comic Sans MS',
+    fontSize: 30,
+    textAlign: 'center',
+    marginTop:25
+  },
+
+  campo:{
+    backgroundColor: '#121212',
+    borderRadius: 10,
+    fontSize: 23,
+    color: '#FFF',
+    margin: 10,
+    padding:10
+  },
+  botao:{
+    backgroundColor:'#41AEF4',
+   alignItems: 'center',
+   justifyContent: 'center',
+   margin: 15,
+   padding:10
+  },
+  TextBotao:{
+    fontSize:25,
+    color:'#FFF'
+  }
 });
